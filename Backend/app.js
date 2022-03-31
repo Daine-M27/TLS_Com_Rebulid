f54d76610104475a2de754fe0f57c1375090ea5f
Daine Marshall <dmarshall@thelightsource.com>
2022-03-31 12:56:53 -0400

    added db connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,30 @@
-import express from "express";
-import bodyParser from "body-parser";
 
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+// load .env information into process.env
+dotenv.config();
+
+// Routers
 import usersRouter from "./routes/users.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(express.json());
+
+// database connection
+try {
+  mongoose.connect(`${process.env.DB_CONNECT}`)
+  console.log('DB Connected')
+} catch (error) {
+  console.log(error)
+}
+
+
 app.use("/users", usersRouter);
 app.get("/", (req, res) => res.send("Welcome"));
 app.all("*", (req, res) => res.send("That route doesnt exist."));
