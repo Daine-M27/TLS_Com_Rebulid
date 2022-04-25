ab8ebe039642f0f0eae72669e8d927573acdb4d2
Daine Marshall <dmarshall@thelightsource.com>
2022-04-25 14:15:07 -0400

    updated parts model

--- Backend/models/part.js
+++ Backend/models/part.js
@@ -5,8 +5,38 @@ const { Schema } = mongoose;
 const partSchema = new Schema(
   {
     name: String,
-    code: String,
-    position: Number,
+    category: String,
+    categoryId: String,
+    categoryPosition: Number,
+    description: String,
+    minimumSize: String,
+    maximumSize: String,
+    workingLoadLimit: String,
+    hardware: String,
+    dimensions: String,
+    weight: String,
+    caseQuantity: String,
+    specSheetFilename: {
+      filename: String,
+      url: String,
+    },
+    installationInstructions: {
+      filename: String,
+      url: String,
+    },
+    orderCodes: [{
+      code: String,
+      finish: String,
+      name: String,
+    }],
+    documents: [{
+      filename: String,
+      url: String,
+    }],
+    photos: [{
+      filename: String,
+      url: String,
+    }]
   },
   { timestamps: true }
 );
