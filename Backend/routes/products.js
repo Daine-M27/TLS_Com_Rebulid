3bd6657672327a451dd1b6c37144bbc538b07bfd
Daine Marshall <dmarshall@thelightsource.com>
2022-04-26 11:36:06 -0400

    all finish routers completed

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -10,7 +10,9 @@ import Part from "../models/part.js";
 // dotenv.config();
 const router = express.Router();
 
-/** Create product or Update if existing category */
+//------------------------Product Category Routes------------------------//
+///////////////////////////////////////////////////////////////////////////
+/** Create product category or Update if existing */
 router.post("/category", verifyUserToken, IsAdmin, async (req, res) => {
   // deconstruct body
   const { name, code, position } = req.body;
@@ -46,10 +48,11 @@ router.get("/categories", (req, res) => {
 
 /** Get Single product category */
 router.get("/category/:categoryId", (req, res) => {
-  Category.findById(req.params.categoryId, (err, doc) => {
+  const categoryId = req.params.categoryId;
+  Category.findById(categoryId, (err, doc) => {
     if (err) {
       console.log(err);
-      res.status(400).send({ error: "Error getting categories from database" });
+      res.status(400).send({ error: "Error getting finish from database" });
     } else {
       res.status(200).send(doc);
     }
@@ -58,7 +61,7 @@ router.get("/category/:categoryId", (req, res) => {
 
 /** Delete product category */
 router.delete("/category/:categoryId", verifyUserToken, IsAdmin, (req, res) => {
-  const categoryId = req.params.categoryId
+  const categoryId = req.params.categoryId;
   Category.findByIdAndDelete(categoryId, (err, doc) => {
     if (err) {
       console.log(err);
@@ -68,10 +71,73 @@ router.delete("/category/:categoryId", verifyUserToken, IsAdmin, (req, res) => {
     }
   });
 
-   //------------------TODO------------------// 
+  //------------------TODO------------------//
   // Add logic to check for products that are in category on delete, remove or alert user that products exsits before deleting.
   // Part.find({category: "categoryId"})
+});
 
+//-------------------------Product Finish Routes-------------------------//
+///////////////////////////////////////////////////////////////////////////
+/** Create product finish or Update if existing */
+router.post("/finish", verifyUserToken, IsAdmin, async (req, res) => {
+  // deconstruct body
+  const { name } = req.body;
+
+  if (name) {
+    Finish.findOneAndUpdate(
+      { name },
+      { name },
+      { new: true, upsert: true },
+      (err, doc) => {
+        if (err) {
+          res.status(400).send({ error: "Error saving to database" });
+        }
+        res.status(200).send({ doc });
+      }
+    );
+  } else {
+    res.status(400).send({ error: "Missing data in request body." });
+  }
+});
+
+/** Get All product finishes */
+router.get("/finishes", async (req, res) => {
+  Finish.find((err, docs) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send({ error: "Error getting finishes from database" });
+    } else {
+      res.status(200).send(docs);
+    }
+  });
+});
+
+/** Get Single product finish */
+router.get("/finish/:finishId", async (req, res) => {
+  const finishId = req.params.finishId;
+  Finish.findById(finishId, (err, doc) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send({ error: "Error getting finish from database" });
+    } else {
+      res.status(200).send(doc);
+    }
+  });
+});
+
+/** Delete product finish */
+router.delete("/finish/:finishId", verifyUserToken, IsAdmin, async (req, res) => {
+  const finishId = req.params.finishId;
+  Finish.findByIdAndDelete(finishId, (err, doc) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(400)
+        .send({ error: "Error deleting category from database" });
+    } else {
+      res.status(200).send(doc);
+    }
+  });
 });
 
 export default router;
